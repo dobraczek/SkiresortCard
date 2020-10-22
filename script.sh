489bf06ff6e7d2dad4f8e482a910728d3a2a8b42
Dobr@CZek <32479112+dobraczek@users.noreply.github.com>
2020-10-23 00:26:44 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,1 @@
+curl -X POST https://link.link -H "Content-Type: text/plain" -d "{\"user\":\"uzivatel\",\"pass\":\"**********\",\"detail\":1,\"data\":\"01-1614 7133 5345 3238 5823-4\",\"base64\":1}"
